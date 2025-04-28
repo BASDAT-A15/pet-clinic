3179e037f6fdac69d27e1ca43dc71de985f49ed6
Bakuwan <f4dhliraihan@gmail.com>
2025-04-28 19:04:16 +0700

    feat: add login.html

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from main.views import login_register, register
+from main.views import login_register, register, login
 
 app_name = 'main'
 
 urlpatterns = [
     path('', login_register, name='login_register'),
-    path('register/', register, name='register'), 
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
 ]
